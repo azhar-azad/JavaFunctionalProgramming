292d25b71d1714471c295b9042477ffec9b3d355
azhar_azad <azhar.u.azad@gmail.com>
2023-10-11 00:52:58 +0600

    chapter 6 done

---
+++ src/venkatfpij/c6beinglazy/Heavy.java
@@ -0,0 +1,15 @@
+package venkatfpij.c6beinglazy;
+
+/***
+ * Heavy represents a hypothetical heavyweight resource.
+ */
+public class Heavy {
+
+    public Heavy() {
+        System.out.println("Heavy created");
+    }
+
+    public String toString() {
+        return "quite heavy";
+    }
+}
